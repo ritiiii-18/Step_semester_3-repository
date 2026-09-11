6a19744a03d1d416a9305f5d23032a45d804cb7a
ritiiii-18 <ritishadixit17@gmail.com>
2026-09-11 23:30:29 +0530

    Create M2.java

---
+++ M2.java
@@ -0,0 +1,45 @@
+class PayrollAccount {
+    private double basicSalary;
+    private double bonus;
+
+    PayrollAccount(double basicSalary) {
+        if (basicSalary < 0) {
+            System.out.println("Warning: Negative salary not allowed. Starting at 0.");
+            this.basicSalary = 0;
+        } else {
+            this.basicSalary = basicSalary;
+        }
+        this.bonus = 0;
+    }
+
+    void creditBonus(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid bonus amount.");
+        } else {
+            bonus += amount;
+            System.out.println("Bonus credited: Rs " + amount);
+        }
+    }
+
+    void deductTax(double percent) {
+        if (percent < 0 || percent > 100) {
+            System.out.println("Invalid tax percentage.");
+        } else {
+            basicSalary -= basicSalary * percent / 100;
+            System.out.println("Tax deducted: " + (int) percent + "%");
+        }
+    }
+
+    double getNetSalary() {
+        return basicSalary + bonus;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        PayrollAccount account = new PayrollAccount(50000);
+        account.creditBonus(5000);
+        account.deductTax(10);
+        System.out.println("Net salary: Rs " + account.getNetSalary());
+    }
+}
