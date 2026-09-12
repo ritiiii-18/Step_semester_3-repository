0a240cc0bf6c9debec89535e887f83803c7fadf8
ritiiii-18 <ritishadixit17@gmail.com>
2026-09-12 11:33:16 +0530

    Create m3.java

---
+++ m3.java
@@ -0,0 +1,35 @@
+class Account {
+    String regNo;
+    double totalFee;
+
+    Account(String regNo, double totalFee) {
+        this.regNo = regNo;
+        this.totalFee = totalFee;
+    }
+
+    final double calculateLateFee(int daysLate) {
+        return totalFee * 0.1 * (daysLate / 20.0);
+    }
+
+    final void printSummary(int daysLate) {
+        if (daysLate <= 0) {
+            System.out.println(regNo + " - On time, no late fee");
+        } else {
+            double lateFee = calculateLateFee(daysLate);
+            System.out.println(regNo + " | Total Fee: Rs " + totalFee + " | Late Fee: Rs " + lateFee);
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String[] regNos = {"RA001", "RA002", "RA003", "RA004"};
+        double[] totalFees = {200000, 150000, 180000, 220000};
+        int[] daysLate = {10, 0, -2, 5};
+
+        for (int i = 0; i < regNos.length; i++) {
+            Account account = new Account(regNos[i], totalFees[i]);
+            account.printSummary(daysLate[i]);
+        }
+    }
+}
