4882b67179fb1b369d7e04e571d731faad92fbed
ritiiii-18 <ritishadixit17@gmail.com>
2026-09-12 11:39:24 +0530

    Create 3.java

---
+++ 3.java
@@ -0,0 +1,47 @@
+public class CineScreen {
+    private int seatsTotal;
+    private int seatsAvailable;
+
+    public CineScreen(int seatsTotal) {
+        if (seatsTotal <= 0) {
+            throw new IllegalArgumentException("seatsTotal must be positive");
+        }
+        this.seatsTotal = seatsTotal;
+        this.seatsAvailable = seatsTotal;
+    }
+
+    void bookSeat() {
+        if (seatsAvailable > 0) {
+            seatsAvailable--;
+        }
+    }
+
+    void cancelBooking() {
+        if (seatsAvailable < seatsTotal) {
+            seatsAvailable++;
+        }
+    }
+
+    int getSeatsAvailable() {
+        return seatsAvailable;
+    }
+
+    public static void main(String[] args) {
+        try {
+            CineScreen bad = new CineScreen(0);
+        } catch (IllegalArgumentException e) {
+            System.out.println("construction rejected");
+        }
+
+        CineScreen c = new CineScreen(2);
+        c.bookSeat();
+        c.bookSeat();
+        c.bookSeat();
+        System.out.println(c.getSeatsAvailable());
+
+        c.cancelBooking();
+        c.cancelBooking();
+        c.cancelBooking();
+        System.out.println(c.getSeatsAvailable());
+    }
+}
